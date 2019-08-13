4bc3b4bc0692019ac34b443401fb749f84eb870b
Alessio Fanelli <fanahova@gmail.com>
2019-08-13 10:29:21 -0400

    Add brew formula (#1)
    * Add brew Formula for initial support
    
    * Install from source
    
    * Move to source building rather than tagged binary download

---
+++ Formula/dolt.rb
@@ -0,0 +1,21 @@
+class Dolt < Formula
+  desc "Dolt - It's git for data"
+  homepage 'https://github.com/liquidata-inc/dolt'
+  url 'https://github.com/liquidata-inc/dolt'
+  sha256 '2c13dfcf594f5c52780ccc97e932bcfd3128b07ff43018e1622a066b4ff36a3a'
+
+  depends_on :go
+
+  bottle :unneeded
+
+  def install
+    system 'cd go'
+    system 'go install ./cmd/dolt'
+    system 'go install ./cmd/git-dolt'
+    system 'go install ./cmd/git-dolt-smudge'
+  end
+
+  test do
+    system "#{bin}/dolt", '--version'
+  end
+end
