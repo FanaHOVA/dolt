0b7fefe3f1c2a7ba22f04e7b565e81c7c7f8e5af
FanaHOVA <fana@alessiofanelli.com>
2019-08-13 17:13:35 -0400

    Install successfully

--- Formula/dolt.rb
+++ Formula/dolt.rb
@@ -1,8 +1,10 @@
 class Dolt < Formula
+  VERSION = '0.9.7'
+
   desc "Dolt - It's git for data"
   homepage 'https://github.com/fanahova/dolt'
-  url 'https://github.com/fanahova/dolt/archive/v0.9.7.tar.gz'
-  version '0.9.7'
+  url "https://github.com/fanahova/dolt/archive/v#{VERSION}.tar.gz"
+  version VERSION
   sha256 '931d1ba53a54d8aef08c2f9f27e16160eea059a3dae1c95ba6ca6760ea7be794'
 
   depends_on 'go' => :build
@@ -10,10 +12,9 @@ class Dolt < Formula
   bottle :unneeded
 
   def install
-    system 'cd go'
-    system 'go install ./cmd/dolt'
-    system 'go install ./cmd/git-dolt'
-    system 'go install ./cmd/git-dolt-smudge'
+    system "cd go && GOBIN=/usr/local/Cellar/dolt/#{VERSION} go install ./cmd/dolt"
+    system "cd go && GOBIN=/usr/local/Cellar/dolt/#{VERSION} go install ./cmd/git-dolt"
+    system "cd go && GOBIN=/usr/local/Cellar/dolt/#{VERSION} go install ./cmd/git-dolt-smudge"
   end
 
   test do
